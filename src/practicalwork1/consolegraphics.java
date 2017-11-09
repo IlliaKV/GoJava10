62532b4571fa28a8c89da8670496b3b6041b4b30
IlliaKozhushko <illyakozshushko@gmail.com>
2017-11-09 21:10:22 +0200

    +input

--- src/practicalwork1/consolegraphics.java
+++ src/practicalwork1/consolegraphics.java
@@ -1,7 +1,19 @@
 package practicalwork1;
+import java.util.Scanner;
 
 public class consolegraphics {
+    public static void drawLine(int x, char c){
+        for(int i=0;i<x;i++)
+        System.out.print(c);
+    }
+    public static char queryChar(){
+        System.out.print("Введите символ: ");
+        Scanner in = new Scanner(System.in);
+        char c = in.nextLine().charAt(0);;
+        return c;
+    }
     public static void main(String[] args) {
-        
+        char symbol = queryChar();
+        drawLine(5, symbol);
     }
 }
