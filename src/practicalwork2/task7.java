5bad41a9ef157add50a081cf8a35d0a2537ceee5
IlliaKozhushko <illyakozshushko@gmail.com>
2017-11-22 17:29:36 +0200

    задание 7 - класс Basic

--- src/practicalwork2/task7.java
+++ src/practicalwork2/task7.java
@@ -1,22 +1,41 @@
 package practicalwork2;
 
 import java.util.Scanner;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+import javax.swing.JPanel;
 
 public class task7 {
     public static void main(String[] args) {
-//         System.out.print("цвет в формате RRGGBB: ");
-//         Scanner in = new Scanner(System.in);
-//         String RRGGBB = in.nextLine();
+//        System.out.print("цвет в формате RRGGBB: ");
+//        Scanner in = new Scanner(System.in);
+//        String RRGGBB = in.nextLine();
 
-        // System.out.println("Hello World!");
-  //      System.out.println("ESC[38;2;4;5;5m");
- //       System.out.println("hello world!");
-
-     //       for(int i = 0; i < 255 ; i++) {
-
+//        System.out.println("Hello World!");
+//        System.out.println("\u001B[31m");
+//        System.out.println("hello world!");
+//        System.out.println("\u001B[38;[5;14m");
+//        System.out.println("hello world!");
+//        for(int i = 0; i < 255 ; i++) {
 //            System.out.println("\u001B[" + i + "m");
-              System.out.println("\u001B[38;2;<44>;<44>;<44>m");
-            System.out.println("hello world!");
-   //     }
+//            System.out.println("\u001B[38;2;<44>;<44>;<44>m");
+//            System.out.println("hello world!");
+//        }
+//        String RRGGBB = "00FF00";
+//        Color rgb = new Color(123,34,54);
+//        String HW = "hello world!";
+//        System.out.println(rgb.);
+
+//        DrawingPanel panel = new DrawingPanel(600,600);
+//        Graphics g = panel.getGraphics();
+//        g.setFont(new Font("Calabri",Font.ITALIC,36));
+//        g.setColor(new Color(255,0,0));
+//        g.drawString("MaeMap tutorial", 100, 100);
+
     }
+
 }
